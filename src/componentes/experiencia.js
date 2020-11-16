9f53106011b76ef62434c57628b9384f38e84e52
Saúl Salazar Méndez <saul.salazar.mendez@gmail.com>
2020-11-16 13:56:28 -0600

    FIX - add estado a empresas

--- src/componentes/experiencia.js
+++ src/componentes/experiencia.js
@@ -13,14 +13,14 @@ const template = /*html*/`
                 <div class="w3-small"><span style="color: rgb(96, 125, 139);">Mas de 10 000 registros</span></div>
                 <div><br></div>
 
-                <div><span style="color: blue;">Front End Developer(Angular) / Evomatik</span><br></div>
+                <div><span style="color: blue;">Front End Developer(Angular) / Evomatik(Veracruz)</span><br></div>
                 <div class="w3-text-green">
                     <i class="fa fa-calendar"></i> Oct 2017 - Mar 2019
                 </div>                
                 <div> 
                     <ul>
                         <li>Encargado del desarrollo y mantenimiento del proyecto de SIGI del estado del México(SEAGeD® Fiscalía).</li>
-                        <li>Utilización de Angular y las últimas tecnologias. </li>
+                        <li>Utilización de Angular y las últimas tecnologías. </li>
                         <li>Desarrollo de herramientas que facilitaron la implementación del front-end lo que implico que nuevos miembros del equipo fueran productivos.</li>
                         <li>Mantener el proyecto de sigi de la fiscalía del estado de México.</li>
                         <li>Como desarrollador estuve encargado de crear componentes, servicios y guards en Angular. Realizar pruebas unitarias.</li>
@@ -28,7 +28,7 @@ const template = /*html*/`
                     </ul>
                 </div><br>
 
-                <div><span style="color: blue;">Web Developer / Evomatik</span><br></div>
+                <div><span style="color: blue;">Web Developer / Evomatik(Veracruz)</span><br></div>
                 <div class="w3-text-green">
                     <i class="fa fa-calendar"></i> Nov 2016 - Jul 2017
                 </div>                
@@ -36,7 +36,7 @@ const template = /*html*/`
                     Desarrollo en Java usando el framaework Play(https://www.playframework.com/) y jquery para el proyecto de gestión de Prices Shoes.
                 </div><br>
 
-                <div><span style="color: blue;">Simulador de procesos / Cimat</span><br></div>
+                <div><span style="color: blue;">Simulador de procesos / Cimat (Guanajuato)</span><br></div>
                 <div class="w3-text-green">
                     <i class="fa fa-calendar"></i> Oct 2007 - Ene 2008
                 </div>                
